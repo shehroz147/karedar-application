62f94e58f78a79e90adacefceee1a7a8c0904903
Hamail Saleemi <hamailsaleemi7740@gmail.com>
2022-06-16 04:00:26 +0500

    testing

--- Component/server.js
+++ Component/server.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
- export const baseUrl = "https://6a60-2400-adc5-17f-8500-3136-e9c6-93be-a4cc.in.ngrok.io/";
-//export const baseUrl = 'https://krayedar.herokuapp.com/'
+ //export const baseUrl = "https://7892-2400-adc5-17f-8500-f995-9654-4357-d17f.in.ngrok.io";
+export const baseUrl = 'https://krayedar.herokuapp.com/'
 export default axios.create({
     baseURL: baseUrl,
 });
